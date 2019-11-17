d05dc061c89de94eb3d13b26d0a2ce4f71a25703
abhimanisubainghe <abhimanisubainghe@gmail.com>
2019-11-17 14:52:45 +0530

    Update cutomer registration

--- FrontEnd/melius/src/components/registerforms/customer/Customerreg.js
+++ FrontEnd/melius/src/components/registerforms/customer/Customerreg.js
@@ -3,13 +3,41 @@ import React from 'react';
 
 export default class Customerreg extends React.Component{
     
+        constructor(props) {
+                super(props)
+            
+                this.state = {
+                    id:"",    
+                    customerName: "",
+                    gender: "",
+                    nic: "",
+                    fax: "",
+                    type: "",
+                    website: "",
+                    email: "",
+                    address: "",
+                    contactNo: "",
+                    dob: "",
+                    note: "",
+                }
+            }
+        
+            onChange = (e) => {
+                this.setState(
+                {[e.target.name]: e.target.value}
+            )
+            }
+            
+            handleSubmit = event => {
+                alert(`customer registered`);
+                
+            }
 
-
-    render(){
-        return(  
-                <div class="jumbotron">  
+        render(){
+                return(  
+                        <div class="jumbotron">  
                 <div className="container">
-                        <form>
+                        <form onSubmit={this.handleSubmit}>
                        
                                 <div className = "col-md-12">
                                         <div className= "row">
@@ -24,34 +52,27 @@ export default class Customerreg extends React.Component{
                                         <hr/> 
 
                                         <div className= "row form-group" align="left">
-                                                <label for="customer_id" className="col-md-2">Customer Id</label>
+                                                <label for="id" className="col-md-2">Customer Id</label>
                                                         <div className="col-md-10">
-                                                                <input type="text" className="form-control" id="vehicle_no" placeholder="Enter customer ID"/>                        
+                                                                <input type="text" value={this.state.id} onChange={this.onChange} className="form-control" id="id" name="id" placeholder="Enter customer ID"/>                        
 
                                                         </div>
                                         </div>
                                         <div className= "row form-group" align="left">
-                                                <label for="customer_id" className="col-md-2">Customer Id</label>
+
+                                                <label for="customerName" className="col-md-2">Customer Name</label>
                                                         <div className="col-md-10">
-                                                                <input type="text" className="form-control" id="vehicle_no" placeholder="Enter customer ID"/>                        
-
-                                                        </div>
-                                        </div>
-
-                                        <div className= "row form-group" align="left">
-
-                                                <label for="customer_name" className="col-md-2">Customer Name</label>
-                                                        <div className="col-md-10">
-                                                                <input type="text" className="form-control" id="owner_name"  placeholder="Cutomer Name"/>  
+                                                                <input type="text" className="form-control" value={this.state.customerName} onChange={this.onChange} id="customerName" name="customerName"  placeholder="Cutomer Name"/>  
                                                                 <fieldset className="row-md form-group">
                                                                         <div className="form-check">
                                                                                 <label class="form-check-label">
                                                                                 <input 
                                                                                         type="radio" 
                                                                                         className="form-check-input" 
-                                                                                        name="optionsRadios" 
-                                                                                        id="optionsRadios1" 
-                                                                                        value="option1" 
+                                                                                        name="gender" 
+                                                                                        id="gender" 
+                                                                                        value={this.state.gender} 
+                                                                                        onChange={this.onChange}
                                                                                         />
                                                                                         Mr.
                                                                                 </label>
@@ -61,9 +82,10 @@ export default class Customerreg extends React.Component{
                                                                                 <input 
                                                                                         type="radio" 
                                                                                         class="form-check-input" 
-                                                                                        name="optionsRadios" 
-                                                                                        id="optionsRadios2" 
-                                                                                        value="option2"/>
+                                                                                        name="gender" 
+                                                                                        id="gender" 
+                                                                                        value={this.state.gender} 
+                                                                                        onChange={this.onChange}/>
                                                                                         Ms.
                                                                                 </label>
                                                                         </div>
@@ -72,9 +94,10 @@ export default class Customerreg extends React.Component{
                                                                                 <input 
                                                                                         type="radio" 
                                                                                         class="form-check-input" 
-                                                                                        name="optionsRadios" 
-                                                                                        id="optionsRadios3" 
-                                                                                        value="option3"/>
+                                                                                        name="gender" 
+                                                                                        id="gender" 
+                                                                                        value={this.state.gender} 
+                                                                                        onChange={this.onChange}/>
                                                                                         Other
                                                                                 </label>
                                                                         </div>
@@ -84,27 +107,59 @@ export default class Customerreg extends React.Component{
                                         <div className= "row form-group" align="left">
                                                 <label for="NIC" className="col-md-2">NIC</label>
                                                         <div className="col-md-10">
-                                                                <input type="text" className="form-control" id="nic" placeholder="NIC"/>                        
+                                                                <input type="text" value={this.state.nic} onChange={this.onChange} className="form-control" id="nic" name="nic" placeholder="NIC"/>                        
 
                                                         </div>
                                         </div>
                                         <div className= "row form-group" align="left">
+                                                <label for="fax" className="col-md-2">Fax</label>
+                                                        <div className="col-md-10">
+                                                                <input type="text" value={this.state.fax} onChange={this.onChange} className="form-control" id="fax" name="fax" placeholder="Fax Number"/>                        
 
+                                                        </div>
+                                        </div>
+                                        <div className= "row form-group" align="left">
+                                                <label for="type" className="col-md-2">Type</label>
+                                                        <div className="col-md-10">
+                                                                <input type="text" value={this.state.type} onChange={this.onChange} className="form-control" id="type" name="type" placeholder="Type"/>                        
+
+                                                        </div>
+                                        </div>
+                                        <div className= "row form-group" align="left">
+                                                <label for="website" className="col-md-2">Website</label>
+                                                        <div className="col-md-10">
+                                                                <input type="text" value={this.state.website} onChange={this.onChange} className="form-control" id="website" name="website" placeholder="Insert website"/>                        
+
+                                                        </div>
+                                        </div>
+                                        <div className= "row form-group" align="left">
                                                 <label for="email" className="col-md-2">E-Mail</label>
                                                         <div className="col">
-                                                                <input type="email" className="form-control" id="email"  placeholder="email"/>                        
+                                                                <input type="email" value={this.state.email} onChange={this.onChange} className="form-control" id="email" name="email"  placeholder="email"/>                        
+                                                        </div>
+                                        </div>
+                                        <div className= "row form-group" align="left">
+                                                <label for="address" className="col-md-2">Address</label>
+                                                        <div className="col">
+                                                                <input type="address" value={this.state.address} onChange={this.onChange} className="form-control" id="address" name="address" placeholder="address"/>                        
+                                                        </div>
+                                        </div>
+                                        <div className= "row form-group" align="left">
+                                                <label for="contactNo" className="col-md-2">Contact no</label>
+                                                        <div className="col">
+                                                                <input type="text" value={this.state.contactNo} onChange={this.onChange} className="form-control" id="contactNo" name="contactNo" placeholder="contact number"/>                        
                                                         </div>
                                         </div>
                                         <div className= "row form-group" align="left">
                                                 <label for="dob" className="col-md-2">DOB</label>
                                                         <div className="col-md">
-                                                                <input type="date" className="form-control" id="date"  placeholder="date of birth"/>                        
+                                                                <input type="date" value={this.state.dob} onChange={this.onChange} className="form-control" id="dob" name="dob"  placeholder="date of birth"/>                        
                                                         </div>
                                         </div>
                                         <div className= "row form-group" align="left">
                                                 <label for="note" className="col-md-2">Note</label>
                                                         <div className="col-md-10">
-                                                        <textarea name="message" rows="3" className="col-md-12" style={textAreaStyle}>
+                                                        <textarea name="message" rows="3" value={this.state.note} onChange={this.onChange} id="note" name="note" className="col-md-12" style={textAreaStyle}>
                                                                 The cat was playing in the garden.
                                                         </textarea>           
                                                         </div>
@@ -116,7 +171,7 @@ export default class Customerreg extends React.Component{
                         </form>
                 </div>
         </div>            
-        )
+                )
     }
         
     
