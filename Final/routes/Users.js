71c03a26b4e6f9e7d20f6c908daed9b823723192
abhimanisubasinghe <abhimanisubasinghe@gmail.com>
2020-02-16 08:27:45 +0530

    live search

--- Final/routes/Users.js
+++ Final/routes/Users.js
@@ -485,6 +485,38 @@ users.post('/search',function(req,res){
     }
 })
 
+//SINGLE USER VIEW
+users.post('/searchByName',function(req,res){
+    console.log("foeiajfej");
+    if(!req.session.userId || !req.session.adminId){
+        var searchId = "%"+req.body.searchId+"%";
+        console.log(searchId);
+        sql.query('select * from user inner join userLogin on user.Id = userLogin.userId WHERE user.name LIKE ?',[searchId],function(err,result){
+            console.log(result);
+            if(err){
+                console.log('err viewuser')
+                console.log(err);
+                throw err;
+            }
+            else{
+                if(result.length>0){
+                    //var state = true;
+                    //var res1 = result[0];
+                    res.json(result);
+                    //res.send({state, res1});
+                }
+                else{
+                    console.log('not work');
+                    res.json(result);
+                }
+            }
+        })
+    }
+    else{
+        res.send('please log');
+    }
+})
+
 //PROFILE
 users.get('/profile', function(req,res){
     console.log('tytytytytyty');
